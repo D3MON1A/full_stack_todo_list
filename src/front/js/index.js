59112119513ddcd6f200270ad4e9b526d2a4ac4f
leisyarcia <leisyarcia@gmail.com>
2020-06-09 00:50:19 +0000

    First commit

--- src/front/js/index.js
+++ src/front/js/index.js
@@ -1,8 +1,3 @@
-/* eslint-disable */
-
-import "../assets/img/rigo-baby.jpg";
-import "../assets/img/4geeks.ico";
-//import 'breathecode-dom'; //DOM override to make JS easier to use
 import "../style/index.scss";
 
 window.onload = function() {
